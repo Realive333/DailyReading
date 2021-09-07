a6b94527df244c4f039d101529c10f3933b041f6
Hibiki "Realive" Akatsuki <realive333@gmail.com>
2021-09-07 21:30:35 +0800

    initial push, create basic function for later test

---
+++ cmd/daily_reading/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/line/line-bot-sdk-go/linebot"
+)
+
+func main() {
+	bot, err := linebot.New(
+		os.Getenv("CHANNEL_SECRET"),
+		os.Getenv("CHANNEL_TOKEN"),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	message := linebot.NewTextMessage("hello, world!")
+	if _, err := bot.BroadcastMessage(message).Do(); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Realive333/DailyReading
+
+go 1.16
+
+require github.com/line/line-bot-sdk-go v7.8.0+incompatible
